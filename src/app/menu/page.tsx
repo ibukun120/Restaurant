f610230cc8e63fb7141bcd868d9bbc254e8014b2
ibukun120 <ibukunoluwawomiloju@gmail.com>
2025-06-08 12:47:42 +0100

    eleven commit

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 export default function MenuPage(){
   return(
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-3rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <div className="p-2 lg:px-20 xl:px-40 h-[calc(100vh-1rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {
         menu.map((category)=>(<Link 
         href={category.slug}
         key={category.id}
-        className="w-full h-1/3 bg-cover p-8 md:h-3/4"
+        className="w-full h-1/3 bg-cover p-5 md:h-3/4"
         style={{backgroundImage: `url(${category.img})`}}>
           <div className={`text-${category.color} w-1/2`}>
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
